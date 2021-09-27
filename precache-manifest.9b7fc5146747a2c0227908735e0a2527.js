e9437d4f3534e95fe6a20619a5aacaa179450ad8
GiomGlobal <90687695+GiomGlobal@users.noreply.github.com>
2021-09-27 14:28:13 +0200

    First release

---
+++ precache-manifest.9b7fc5146747a2c0227908735e0a2527.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec25aeb43311c78d4b5",
+    "url": "/css/app.df01dee0.css"
+  },
+  {
+    "revision": "5d8706f99836f66bba3a",
+    "url": "/css/chunk-vendors.7515e10f.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "575da10a05be610bb36653baf8f8ea60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5317ac426405f3bd1eaa",
+    "url": "/js/about.c2d630c3.js"
+  },
+  {
+    "revision": "4ec25aeb43311c78d4b5",
+    "url": "/js/app.6bb46608.js"
+  },
+  {
+    "revision": "5d8706f99836f66bba3a",
+    "url": "/js/chunk-vendors.32cd3653.js"
+  },
+  {
+    "revision": "19a37550da7f7904088331123c5dfe34",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
